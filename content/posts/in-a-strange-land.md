7190fc618a60f37a2a95b7e4611f5d37cddf31b6
Austin Pocus <ajpocus@gmail.com>
2020-12-13 16:07:36 -0500

    auto deploy magic stuff

---
+++ content/posts/in-a-strange-land.md
@@ -0,0 +1,6 @@
+---
+title: "In a Strange Land"
+date: 2020-12-13T15:45:09-05:00
+draft: true
+---
+
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-hugo && hugo deploy && aws cloudfront create-invalidation --distribution=ELJZDBLCTCBH2 --paths="/*"
+hugo
+hugo deploy
+aws cloudfront create-invalidation --distribution=ELJZDBLCTCBH2 --paths="/*" >&1
